5e73c81cc67338333db1bb8c00bcfab2fd308153
Jacky <gomjellie@gmail.com>
2020-06-25 06:04:25 +0900

    week2 lecture

---
+++ Concentration/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Jacky on 2020/06/25.
+//  Copyright © 2020 Jacky. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return Card.identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
