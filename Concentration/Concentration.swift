3240f9d3d062544189ea6986a7f5bda7d64f2aac
Jacky <gomjellie@gmail.com>
2020-06-26 23:37:47 +0900

    add theme colors

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -1,26 +1,36 @@
-//
-//  Concentration.swift
-//  Concentration
-//
-//  Created by Jacky on 2020/06/25.
-//  Copyright © 2020 Jacky. All rights reserved.
-//
-
 import Foundation
 
 
 class Concentration {
     var cards =  [Card]()
+    var score = 0
+    var cardLog = [Int: Set<Int>]() // identifier -> [index]
     
     var indexOfOneAndOnlyFaceUpCard: Int?
     
     func chooseCard(at index: Int) {
+        let id = cards[index].identifier
+        if let logOfId = cardLog[id] {
+            cardLog[id] = logOfId.union([index])
+        } else {
+            cardLog[id] = [index]
+        }
+        
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 // check if card match
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
+                    score += 2
+                }
+                else {
+                    if cardLog[id]!.count == 2 {
+                        score -= 1
+                    }
+                    if cardLog[cards[matchIndex].identifier]?.count == 2 {
+                        score -= 1
+                    }
                 }
                 cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = nil
@@ -40,6 +50,13 @@ class Concentration {
             let card = Card()
             cards += [card, card]
         }
-        // TODO: Shuffle the cards (HW)
+        for _ in 1...numberOfPairsOfCards * 2 {
+            let randomCardIndex = Int(arc4random_uniform(UInt32(numberOfPairsOfCards * 2)))
+            let cardIndexToSwap = Int(arc4random_uniform(UInt32(numberOfPairsOfCards * 2)))
+            
+            let tempCard = cards[randomCardIndex]
+            cards[randomCardIndex] = cards[cardIndexToSwap]
+            cards[cardIndexToSwap] = tempCard
+        }
     }
 }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -1,37 +1,67 @@
-//
-//  ViewController.swift
-//  Concentration
-//
-//  Created by Jacky on 2020/06/21.
-//  Copyright © 2020 Jacky. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    var game = Concentration(numberOfPairsOfCards: 4)
+    var game = Concentration(numberOfPairsOfCards: 8) {
+        didSet {
+            flipCount = 0
+        }
+    }
     
     var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
+    
+    var gameScore = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(gameScore)"
+        }
+    }
 
     @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
     
     @IBOutlet var cardButtons: [UIButton]!
     
     override func viewDidLoad() {
-        game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1 / 2))
+        newGame()
     }
     
+    @IBOutlet var backGroundUIView: UIView!
     
     @IBAction func touchCard(_ sender: UIButton) {
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            let card = game.cards[cardIndex]
+            if card.isMatched {
+                return
+            }
+        }
+        
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
         }
+    }
+    
+    @IBOutlet weak var newGameButton: UIButton!
+    
+    @IBAction func onTouchNewGame(_ sender: UIButton) {
+        newGame()
+    }
+    
+    func newGame() {
+        game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+        (theme, _) = themes.randomElement()!
+        emoji.removeAll()
+        backGroundUIView.backgroundColor = backGroundColor
+        flipCountLabel.textColor = primaryColor
+        scoreLabel.textColor = primaryColor
+        newGameButton.setTitleColor(primaryColor, for: UIControl.State.normal)
+        
+        updateViewFromModel()
     }
     
     func updateViewFromModel() {
@@ -43,16 +73,62 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : primaryColor
             }
+        }
+        gameScore = game.score
+    }
+    
+    var emojiChoices = ["👻", "🎃", "🦇", "😈", "🕷", "🌜", "🔥", "🍭", "🍷"]
+    
+    var theme = "halloween" {
+        didSet {
+            emojiChoices = themes[theme]!["icons"] as! [String]
+            backGroundColor = themes[theme]!["backGroundColor"] as! UIColor
+            primaryColor = themes[theme]!["primaryColor"] as! UIColor
         }
     }
     
-    var emojiChoices = ["👻", "🎃", "🦇", "🧸", "🍎", "👮🏻‍♀️"]
+    var backGroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    var primaryColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     
+    var themes = [
+        "halloween": [
+            "icons": ["👻", "🎃", "🦇", "😈", "🕷", "🌜", "🔥", "🍭", "🍷"],
+            "backGroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
+            "primaryColor": #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),
+        ],
+        "sports": [
+            "icons": ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🏄‍♂️", "🏉", "🏓", "⛳️"],
+            "backGroundColor": #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
+            "primaryColor": #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),
+        ],
+        "animals": [
+            "icons": ["🐶", "🦊", "🐼", "🦁", "🐷", "🐒", "🐔", "🐣", "🦄"],
+            "backGroundColor": #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
+            "primaryColor": #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1),
+        ],
+        "nature": [
+            "icons": ["🌈", "⭐️", "🌤", "🌧", "❄️", "☔️", "🌬", "🌊", "⚡️"],
+            "backGroundColor": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+            "primaryColor": #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),
+        ],
+        "faces": [
+            "icons": ["😂", "😈", "😘", "😎", "🤓", "😤", "😡", "😱", "🤔"],
+            "backGroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
+            "primaryColor": #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),
+        ],
+        "flag": [
+            "icons": ["🇨🇦", "🇩🇪", "🇯🇵", "🇸🇬", "🇰🇷", "🇺🇸", "🇨🇳", "🇦🇺", "🏁"],
+            "backGroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
+            "primaryColor": #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1),
+        ],
+    ]
+
     var emoji =  [Int:String]()
     
     func emoji(for card: Card) -> String {
+        print("card.identifier : \(card.identifier)")
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
